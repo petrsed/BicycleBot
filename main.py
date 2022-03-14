25b3a53ab2c81d0e2136c8e005f156d085caf770
petiy <s>
2022-03-14 12:45:08 +0700

    add description

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from messages import *
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(bot)
 
+async def send_main_keyboard(chat_id, text=MAIN_KEYBOARD_MESSAGE):
+    await bot.send_message(chat_id, text)
+
 
 @dp.callback_query_handler()
 async def query_show_list(call: types.CallbackQuery):
@@ -19,8 +22,11 @@ async def query_show_list(call: types.CallbackQuery):
 async def send_welcome(message: types.Message):
     logging_message(message.chat.id, message.from_user.username, message.text)
     chat_id, username, text = message.chat.id, message.from_user.username, message.text
+    await send_main_keyboard(chat_id, HELLO_MESSAGE)
     if not check_user_presence(chat_id):
         create_user(chat_id, username)
+        await bot.send_message(chat_id, WELCOME_MESSAGE)
+
 
 
 @dp.message_handler()
